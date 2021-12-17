76920d2a395d832742472f6af5fe15cfd5a1eeb3
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2021-12-16 22:51:50 -0800

    Using async/await instead of then/catch

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -93,18 +93,15 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = (body) => async (dispatch) => {
   try {
-    saveMessage(body)
-    .then((data) =>{
-      if (!body.conversationId) {
-        dispatch(addConversation(body.recipientId, data.message));
-      } else {
-        dispatch(setNewMessage(data.message));
-      }
-      sendMessage(data, body);
-    });
-    console.log("send message done!!")
+    const data = await saveMessage(body);
+    if (!body.conversationId) {
+      dispatch(addConversation(body.recipientId, data.message));
+    } else {
+      dispatch(setNewMessage(data.message));
+    }
+    sendMessage(data, body);
   } catch (error) {
     console.error(error);
   }
